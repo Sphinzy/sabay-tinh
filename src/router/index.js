7468adb5a41366350efcba56df54eaf024cf7a26
Paoheang <paoheang.ccf87121@gmail.com>
2026-02-19 16:09:29 +0700

    pull

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
+import Login from "@/views/Login.vue";
 import { createRouter, createWebHistory } from "vue-router";
+
 // import { useAuthStores } from '@/stores/auth'
 
 const routes = [
@@ -8,18 +10,30 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes
+    routes : [
+
+     {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    ],
+
+
 });
 
-// router.beforeEach((to, from) => {
-//   const auth =useAuthStores();
 
-//   if (!auth.isLoggedIn && to.meta.require) {
-//     return {name: 'login'}
-//   }
+   
 
-//   return true;
+router.beforeEach((to, from) => {
+  const auth =useAuthStores();
 
-// })
+  if (!auth.isLoggedIn && to.meta.require) {
+    return {name: 'login'}
+  }
+
+  return true;
+
+})
 
 export default router
