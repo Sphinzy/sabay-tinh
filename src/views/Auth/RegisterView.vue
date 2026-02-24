3ad0f3fabeaf6a09d97affeef114dab8e7a5920a
Paoheang <paoheang.ccf87121@gmail.com>
2026-02-24 14:39:08 +0700

    push

--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,64 +1,84 @@
 <template>
-  <main class="bg-main">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-6">
-          <form @submit.prevent="handleRegister">
-            <h2 class="text-white mb-3">Create Account</h2>
+  <div style="height: 100vh;">
+    <main class="bg-main">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <form @submit.prevent="handleRegister" class="bg-main">
+              <h1 style="padding-top: 40px; padding-left: 10px;">Create an account</h1>
 
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="text-white">Email</label>
-              <input v-model="form.email" type="text" class="form-control" />
-              <small class="text-danger">{{ errors.email }}</small>
+              <div class="row">
+
+                <div class="col-12">
+                  <label class="custom-label" style="color: white;">
+                    <i class="bi bi-envelope"></i> Email
+                  </label>
+                  <input  v-model="form.email" type="text" placeholder="Enter your Email" class="form-control mt-1">
+                  <p v-if="errors.email" class="text-danger">{{ errors.email }}</p>
+                </div>
+
+                <div class="col-12">
+                  <label class="custom-label" style="color: white;">
+                    <i class="bi bi-person"></i> Name
+                  </label>
+                  <input v-model="form.name" type="text" placeholder="Enter your Name" class="form-control mt-1">
+                  <p v-if="errors.name" class="text-danger">{{ errors.name }}</p>
+                </div>
+
+
+                <div class="col-12">
+                  <label class="custom-label" style="color: white;">
+                    <i class="bi bi-lock"></i> Password
+                  </label>
+                  <input v-model="form.password" type="password" placeholder="Enter your Password"
+                    class="form-control mt-1">
+                  <p v-if="errors.password" class="text-danger">{{ errors.password }}</p>
+                </div>
+
+                <div class="col-12">
+                  <label class="custom-label" style="color: white;">
+                    <i class="bi bi-lock-fill"></i> Confirm Password
+                  </label>
+                  <input v-model="form.confirmPassword" type="password" placeholder="Enter your Confirm Password"
+                    class="form-control mt-1">
+                    <span class="password-eye" @click="togglePassword" >
+                                                <i :class="showPassword ? 'bi bi-eye text-main' : 'bi bi-eye-slash text-main'"></i>
+                                            </span>
+                  <p v-if="errors.confirmPassword" class="text-danger">{{ errors.confirmPassword }}</p>
+                </div>
+
+                <!-- Submit Button -->
+                <div class="col-12 mt-3">
+                  <button  :disabled="isLoading"  type="submit" class="btn btn-primary">  
+                    <div v-if="isLoading" class="spinner-border spinner-border-sm fw-medium"
+                                                role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                            <div v-else>
+                                                Sign up
+                                            </div></button>
+                </div>
+
+                <!-- Login Link -->
+                <p class="text-center mt-4 mb-0">
+                  Already have an account?
+                  <RouterLink to="/login" class="text-decoration-none">Login</RouterLink>
+                </p>
+
+              </div>
+            </form>
+          </div>
+
+          <div class="col-6">
+            <div class="img">
+              <img src="../../assets/image/2.png" alt="">
             </div>
+          </div>
 
-            <!-- Name -->
-            <div class="mb-3">
-              <label class="text-white">Name</label>
-              <input v-model="form.name" type="text" class="form-control" />
-              <small class="text-danger">{{ errors.name }}</small>
-            </div>
-
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="text-white">Password</label>
-              <input
-                v-model="form.password"
-                type="password"
-                class="form-control"
-              />
-              <small class="text-danger">{{ errors.password }}</small>
-            </div>
-
-            <!-- Confirm -->
-            <div class="mb-3">
-              <label class="text-white">Confirm Password</label>
-              <input
-                v-model="form.confirmPassword"
-                type="password"
-                class="form-control"
-              />
-              <small class="text-danger">{{ errors.confirmPassword }}</small>
-            </div>
-
-            <button :disabled="isLoading" class="btn btn-primary w-100">
-              <span
-                v-if="isLoading"
-                class="spinner-border spinner-border-sm"
-              ></span>
-              <span v-else>Register</span>
-            </button>
-
-            <p class="text-center mt-3">
-              Already have account?
-              <RouterLink to="/login">Login</RouterLink>
-            </p>
-          </form>
         </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </div>
 </template>
 
 <script setup>
@@ -112,4 +132,90 @@ async function handleRegister() {
     isLoading.value = false;
   }
 }
-</script>
+</script> 
+
+<style scoped>
+.password-eye {
+    position: absolute;
+    top: 50%;
+    right: 15px;
+    transform: translateY(-50%);
+    cursor: pointer;
+    color: #6c757d;
+    z-index: 5;
+}
+
+.password-eye:hover {
+    color: #000;
+}
+
+img{
+    margin-top: 20px;
+    width: 100%;
+    height: 660px;
+    object-fit: cover;
+    margin-right: -80px;
+}
+.text-danger {
+  color: #ff6b6b;
+  font-size: 13px;
+  margin-top: 4px;
+}
+
+h1 {
+  font-weight: 600;
+  color: white;
+}
+
+button {
+  width: 100%;
+  margin-top: 10px;
+}
+
+input {
+  width: 100%;
+  border-radius: 5px;
+
+}
+
+.col-12 {
+  padding-left: 20px;
+}
+
+form {
+  margin-top: 20px;
+  border: 2px solid rgb(0, 64, 255);
+  width: 100%;
+  max-width: 500px;
+  background-color: rgb(92, 92, 92);
+  padding: 20px;
+}
+
+.custom-label {
+  display: inline-block;
+  margin-top: 14px;
+  margin-bottom: 4px;
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: 0.03em;
+  padding: 4px 10px;
+  border-left: 3px solid #63ff85;
+  border-radius: 4px;
+}
+
+.custom-label i {
+  color: #e6e6eb;
+  margin-right: 5px;
+}
+input:hover {
+  border-color: #100351d9;
+  box-shadow: 0 4px 12px rgba(42, 116, 245, 0.3);
+}
+input {
+  padding: 12px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  transition: all 0.3s ease;   /* important for smooth effect */
+}
+
+</style>
